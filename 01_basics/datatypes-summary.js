59502d8e349e46bac3aabe579494a8a6d4b5ab8d
Princeyadav29 <py291105@gmail.com>
2026-03-14 07:20:36 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//  Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = Null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+// const bigNumber = 3456597463131646444646n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "prince"
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
