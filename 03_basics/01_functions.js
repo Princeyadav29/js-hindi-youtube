d3b29a89f9cd23708644aba7784fc579d3986103
Princeyadav29 <py291105@gmail.com>
2026-03-26 09:27:18 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,35 @@ function loginUserMessage (username = "Sam"){
 }
 
 // console.log(loginUserMessage("Prince"));
-console.log(loginUserMessage("Prince"));
+// console.log(loginUserMessage("Prince"));
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Prince",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and Price is ${anyobject.price}`);   
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));
