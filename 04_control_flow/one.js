de93ac48c4bba44d9ca5eb7affc5e597dc9422b3
Princeyadav29 <py291105@gmail.com>
2026-03-28 10:22:30 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+// if
+const isUserLoggedIn = true
+const temperature =41
+
+// if (temperature === 41){
+//     console.log("less than 50");
+// }else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
