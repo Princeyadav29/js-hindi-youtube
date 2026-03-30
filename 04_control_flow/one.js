1bbf01ffd2c1727fa511ec329b31baa93cec8a8d
Princeyadav29 <py291105@gmail.com>
2026-03-30 11:07:26 +0000

    contol flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -14,9 +14,41 @@ const temperature =41
 // const score = 200
 
 // if (score > 100) {
-//     const power = "fly"
+//     let power = "fly"
 //     console.log(`user power: ${power}`);
 // }
 
 // console.log(`user power: ${power}`);
 
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2"); nahi karna hai is tarah se
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// }else if (balance < 900){
+//     console.log("less than 1200");
+    
+// }else {
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const LoggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3 ) {
+    console.log("Allow to buy course");
+    
+}
+
+if (loggedInFromGoogle || LoggedInFromEmail) {
+    console.log("User Logged in");
+    
+}
